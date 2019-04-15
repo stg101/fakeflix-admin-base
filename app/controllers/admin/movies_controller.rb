2c8a69816a8dff8f3c4bf6b8f8945ff17df5e063
Ricardo <ry.yrupailla@gmail.com>
2019-04-15 15:49:36 -0500

    List rentals

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -4,13 +4,37 @@ module Admin
       @movies = Movie.all
     end
 
+    def show
+      @movie = Movie.find(params[:id])
+    end
+
+    def edit
+      @movie = Movie.find(params[:id])
+    end
+
+    def destroy
+      movie = Movie.find(params[:id])
+      movie.destroy
+      redirect_to admin_movies_path, notice: "The movie was successfully deleted"
+    end
+
+    def new
+      @movie = Movie.new
+    end
+
+    def create
+      @movie = Movie.new(movie_params)
+      @movie.save
+      redirect_to admin_movie_path(@movie), notice: "The artist was successfully created"
+    end
+
     def update
       movie = Movie.find(params[:id])
-      movie.update(user_params)
+      movie.update(movie_params)
       render json: movie.attributes.to_json, status: :ok
     end  
 
-    def user_params
+    def movie_params
       params.require(:movie).permit(
         :title,
         :description,
--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :admin do
 
+    root to: "home#index"
     resources :movies
     resources :series
-    resources :rentals
-    
+    resources :rentals    
 
   end
+  root to: "admin/home#index"
 end
